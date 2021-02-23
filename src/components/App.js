c8582b9cfdb8979db35dc47f0e61cb0d80f0a542
indyteo <theo.mail@laposte.net>
2021-02-23 22:52:00 +0100

    Added list name personalisation & some style

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import ItemsList from "./ItemsList";
 import Footer from "./Footer";
 import {useState} from "react";
 
-const database = JSON.parse(localStorage.getItem("items")) || [
+const _items = JSON.parse(localStorage.getItem("items")) || [
 	{
 		id: 1,
 		name: "Learn React",
@@ -22,22 +22,30 @@ const database = JSON.parse(localStorage.getItem("items")) || [
 		checked: false
 	}
 ];
-let i = database.map(item => item.id).reduce((a, b) => Math.max(a, b), 0);
+let i = _items.map(item => item.id).reduce((a, b) => Math.max(a, b), 0);
+const _appName = localStorage.getItem("appName") || "React Learning";
+document.title = `${_appName} - React TODO App`;
 
 function App() {
-	const [items, _setItems] = useState(database);
-	const setItems = (args) => {
-		_setItems(args);
-		localStorage.setItem("items", JSON.stringify(args));
+	const [items, _setItems] = useState(_items);
+	const setItems = (newItems) => {
+		_setItems(newItems);
+		localStorage.setItem("items", JSON.stringify(newItems));
 	};
 	const addItem = () => setItems([...items, {id: ++i, name: "", checked: false}]);
 	const setItem = (id, name) => setItems(items.map(item => item.id === id ? {...item, name: name} : item));
 	const checkItem = (id, checked) => setItems(items.map(item => item.id === id ? {...item, checked: checked} : item));
 	const deleteItem = id => setItems(items.filter(item => item.id !== id));
 	const checkAll = checked => setItems(items.map(item => ({...item, checked: checked})));
+	const [appName, _setAppName] = useState(_appName);
+	const setAppName = (newAppName) => {
+		_setAppName(newAppName);
+		document.title = `${newAppName} - React TODO App`;
+		localStorage.setItem("appName", newAppName);
+	};
 	return <div className="app">
 		<div className="container">
-			<Header items={items} setItems={setItems} checkAll={checkAll} />
+			<Header setItems={setItems} checkAll={checkAll} appName={appName} setAppName={setAppName} />
 			<ItemsList items={items} addItem={addItem} setItem={setItem} checkItem={checkItem} deleteItem={deleteItem} />
 			<Footer />
 		</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import "../styles/Header.css";
+import {FaGithub} from 'react-icons/fa';
 import Menu from "./Menu";
 
-function Header({setItems, checkAll}) {
+function Header({setItems, checkAll, appName, setAppName}) {
 	return <header className="header d-flex justify-content-center align-items-center position-relative">
 		<Menu>
 			<p onClick={() => setItems([])}>Clear list</p>
 			<p onClick={() => checkAll(true)}>Check all</p>
 			<p onClick={() => checkAll(false)}>Uncheck all</p>
 		</Menu>
-		<h1 className="header-title">React learning</h1>
+		<input className="header-title" value={appName} onChange={e => setAppName(e.target.value)} size={appName.length + 1} />
+		<a className="git-icon" href="https://github.com/indyteo/react-todo-app" target="_blank" rel="noreferrer"><FaGithub size="2em" /></a>
 	</header>;
 }
 
